13db37b7ad544c2871d5711aff2b0529c805ac9b
Vuk Mirovic <wooque@gmail.com>
2015-01-01 23:11:45 +0100

    Firstly executed scheme form JSON file

--- src/hw_sim/core.clj
+++ src/hw_sim/core.clj
@@ -2,11 +2,8 @@
   (:require [clojure.string :refer [join]]
             [cheshire.core :as json :refer [parse-string]])
   (:gen-class))
-  
-(set! *warn-on-reflection* true)
 
-; record for Pin with its state and connected components aka children
-(defrecord Pin [value children])
+(set! *warn-on-reflection* true)
 
 (defn get-pin-value
   "Gets pin value from given components and pin name."
@@ -18,18 +15,15 @@
   [components pin-names]
   (if (coll? (first pin-names))
     (map #(get-pin-value components %) pin-names)
-    (cons (get-pin-value components pin-names) '()) ))
-
-(defprotocol LogicComponent
-  "Protocol of logic component"
-  (logic-func [this components] "Function logic component implements"))
+    (-> (get-pin-value components pin-names) 
+        (cons '() ))))
 
 (defn get-comp-children
   "Gets all compoenents theat are connected on output of component"
   [comp]
   (get-in comp [:out :children]))
 
-(defn simple-logical-func
+(defn simple-logic-func
   "Simplest logical function.
   Get all :in pins, apply function and set output to :out pin"
   [func comp comps]
@@ -40,33 +34,15 @@
    
    (get-comp-children comp)])
 
-(defmacro defcomponent
-  [kind func & additional-pins]
-  `(defrecord ~kind [~'in ~'out ~@additional-pins]
-     LogicComponent
-     (logic-func [this# components#]
-       (~func this# components#))))
-
-; definition for AND, OR, XOR logic components
-(defcomponent AND (partial simple-logical-func bit-and))
-(defcomponent OR (partial simple-logical-func bit-or))
-(defcomponent XOR (partial simple-logical-func bit-xor))
-
 (defn not-func
   "Logical NOT function"
   [value]
   (if (= value 0) 1 0))
-  
-; definition for NOT logical component
-(defcomponent NOT (partial simple-logical-func not-func))
 
 (defn bit-func
   "BIT function, returns unmodified component and value of output pin"
   [comp comps]
   [comp (get-comp-children comp)])
-
-; Standalone Pin definition
-(defcomponent BIT bit-func)
 
 (defn simple-reg-func
   "Simple register function"
@@ -78,8 +54,14 @@
    
    (get-comp-children comp)])
 
-; definition for REG logic component
-(defcomponent REG simple-reg-func ld clk)
+(defmulti logic-func (fn [comp comps] (:type comp)))
+
+(defmethod logic-func :and [comp comps] (simple-logic-func bit-and comp comps))
+(defmethod logic-func :or [comp comps] (simple-logic-func bit-or comp comps))
+(defmethod logic-func :xor [comp comps] (simple-logic-func bit-xor comp comps))
+(defmethod logic-func :not [comp comps] (simple-logic-func not-func comp comps))
+(defmethod logic-func :bit [comp comps] (bit-func comp comps))
+(defmethod logic-func :reg [comp comps] (simple-reg-func comp comps))
 
 (defn exec-logic-func
   "Execute component logic function"
@@ -91,11 +73,11 @@
 
 (defn get-components-by-property
   "Gets components from components map by property"
-   [comps property]
-   (->> comps
-        (filter #(property (val %)))
-        (keys)
-        (into [])))
+  [comps property]
+  (->> comps
+       (filter #(property (val %)))
+       (keys)
+       (into [])))
 
 (defn get-next-comp
   "Gets next component for execution"
@@ -146,52 +128,97 @@
 (defn load-scheme-data
   "Load hardware scheme from JSON file"
   [file]
-    (-> (slurp file)
-        (json/parse-string true)))
+  (-> (slurp file)
+      (json/parse-string true)))
 
 (defn make-comp-name
   "Makes component name in format parent/component-name"
   [parent component]
-    (if parent
-      (str parent "/" component)
-      component))
+  (if parent
+    (str parent "/" component)
+    component))
       
 (defn norm-comp-name
   "For names from local scheme put prefix, also keywordize all names"
   [^String comp-name scheme]
-    (-> (if (.contains comp-name "/")
-          (comp-name)
-          (make-comp-name scheme comp-name))
-       
-        (keyword)))
+  (-> (if (.contains comp-name "/")
+        (comp-name)
+        (make-comp-name scheme comp-name))
+     
+      (keyword)))
+
+(defn process-pin
+  [pin-data scheme]
+  (let [[pin-path pin-name] pin-data
+        norm-pin-path (norm-comp-name pin-path scheme)
+        norm-pin-name (keyword pin-name)]
+    
+    [norm-pin-path norm-pin-name]))
+
+(defn process-pins 
+  [pin-data scheme]
+  (when pin-data
+    (if (coll? (first pin-data))
+      (-> (map #(process-pin % scheme) pin-data)
+          (into []))
+      (process-pin pin-data scheme))))
+        
+(defn process-simple-comp
+  [comp-type comp-data scheme comps]
+  (let [{comp-name :name input :in output :out} comp-data
+        in-pins (process-pins input scheme)
+        out-pins (map #(norm-comp-name % scheme) output)
+        component-init {:type comp-type
+                        :out {:value 1 :children (set out-pins)}}
+        component (if in-pins
+                    (assoc component-init :in in-pins)
+                    component-init)
+        norm-name (norm-comp-name comp-name scheme)]
+    
+    [norm-name component]))
 
 ; function for making components from raw data 
 (defmulti process-comp (fn [component scheme comps] (:type component)))
 
 (defmethod process-comp "scheme"
   [scheme parent-scheme comps]
-    (let [scheme-prefix (make-comp-name parent-scheme (:name scheme))]
-      (->> (:components scheme)
-           (reduce #(process-comp %2 scheme-prefix %1) comps))))
+  (let [scheme-prefix (make-comp-name parent-scheme (:name scheme))]
+    (->> (:components scheme)
+         (reduce #(process-comp %2 scheme-prefix %1) comps))))
     
 (defmethod process-comp "not"
   [not-data scheme comps]
-    (let [input (:in not-data)
-          in-comp (norm-comp-name (:component input) scheme)
-          in-pin (norm-comp-name (:pin input) scheme)
-          out-pin (map #(norm-comp-name % scheme) (:out not-data))
-          not-comp (->NOT [in-comp in-pin] (->Pin 1 (set out-pin)))]
-      
-      (->> (norm-comp-name (:name not-data) scheme)
-           (#(assoc comps % not-comp)))))
+  (let [[comp-name component] (process-simple-comp :not not-data scheme comps)]
+    (assoc comps comp-name component)))
+
+(defmethod process-comp "and"
+  [and-data scheme comps]
+  (let [[comp-name component] (process-simple-comp :and and-data scheme comps)]
+    (assoc comps comp-name component)))
+
+(defmethod process-comp "or"
+  [or-data scheme comps]
+  (let [[comp-name component] (process-simple-comp :or or-data scheme comps)]
+    (assoc comps comp-name component)))
+
+(defmethod process-comp "xor"
+  [xor-data scheme comps]
+  (let [[comp-name component] (process-simple-comp :xor xor-data scheme comps)]
+    (assoc comps comp-name component)))
     
 (defmethod process-comp "bit"
   [bit-data scheme comps]
-    comps)
+  (let [[comp-name component] (process-simple-comp :bit bit-data scheme comps)]
+    (assoc comps comp-name component)))
     
 (defmethod process-comp "reg"
   [reg-data scheme comps]
-    comps)
+  (let [[comp-name component] (process-simple-comp :reg reg-data scheme comps)
+        ld-pin (process-pin (:ld reg-data) scheme)]
+        
+    (-> (assoc component :ld ld-pin)
+        (assoc :clk 1)
+        (#(assoc comps comp-name %)))))
 
 (defn comps-to-string
   "Makes string from map of components"
@@ -211,24 +238,14 @@
        (join "\n--------------\n")
        (println)))
 
-;(def comps (atom {:bit_one (->BIT nil
-                                  ;(->Pin 1 #{:reg_main}))
-                  ;
-                  ;:reg_neg (->NOT [:reg_main :out]
-                                  ;(->Pin 1 #{:reg_main}))
-                  ;
-                  ;:reg_main (->REG [:reg_neg :out]
-                                   ;(->Pin 0 #{:reg_neg})
-                                   ;[:bit_one :out] 1)}))
-;(def history (atom [@comps]))
-;
-;(exec-num-clks 5 comps history)
-;
-;(print-history history)
 
 (def scheme-data (load-scheme-data "schemes/schemes.json"))
 (def scheme (process-comp scheme-data nil {}))
-(println scheme)
+(def comps (atom scheme))
+(def history (atom [@comps]))
+
+(exec-num-clks 5 comps history)
+(print-history history)
 
 (defn -main
   "Main"
